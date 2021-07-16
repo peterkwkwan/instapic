db972efaccccf1c5ec3e76a5dd9302878dcce1c2
peterkwkwan <peterkwkwan@gmail.com>
2021-07-16 21:17:45 +0800

    Initial commit

---
+++ src/actions/posts.js
@@ -0,0 +1,22 @@
+import * as api from '../api';
+
+// Action Creators - functions that return actions
+// Actions - contain type of action and data
+// Use Redux Thunk to handle async actions --> dispatch action instead of returning it
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+        dispatch({type: 'CREATE', payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
